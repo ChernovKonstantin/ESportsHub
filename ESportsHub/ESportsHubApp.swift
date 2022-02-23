ab94cdccdd0c5ab644027c43fad9205b45d5a05a
Chernov Kostiantyn <chernov.konstantin@gmail.com>
2022-02-23 12:10:00 +0200

    Initial Commit

---
+++ ESportsHub/ESportsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESportsHubApp.swift
+//  ESportsHub
+//
+//  Created by Константин Чернов on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ESportsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
